ca475d0d83949609048ec7d8fbfaf2cf789063df
itzcookiie <mayowaadeniyi@hotmail.com>
2018-10-23 17:00:04 +0100

    Done

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-require 'pry'
 def meal_choice(food = "meat")
   return food
-  binding.pry
 end
